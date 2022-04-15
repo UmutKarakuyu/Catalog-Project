133a2ac807a9abfa6ebc822d6fa4362d4671b449
SurceX <85957493+SurceX@users.noreply.github.com>
2022-04-15 19:35:53 +0300

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+public class Item {
+
+    /* It was created as a prototype.
+     The code blocks will be filled in soon.
+
+     */
+
+    private Type type;
+    private ArrayList<Tag> tags;
+    private ArrayList<Property> properties;
+
+    public Item(Type type, ArrayList<Tag> tags, ArrayList<Property> properties) {
+        this.type = type;
+        this.tags = tags;
+        this.properties = properties;
+    }
+
+    public void createProperty(){
+
+    }
+
+    public void createTag(){
+
+    }
+
+    public void editProperty(){
+
+    }
+
+    public void editTag(){
+
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public ArrayList<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(ArrayList<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public ArrayList<Property> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(ArrayList<Property> properties) {
+        this.properties = properties;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "type=" + type +
+                ", tags=" + tags +
+                ", properties=" + properties +
+                '}';
+    }
+}
